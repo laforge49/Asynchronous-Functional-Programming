416f235a9ee74fa2c9c2d17533e46d5543ae583c
Bill LaForge <laforge49@gmail.com>
2011-07-04 08:03:35 +0530

    updated nav map seq

--- Util/src/main/scala/org/agilewiki/util/lite/seq/LiteNavigableMapSeq.scala
+++ Util/src/main/scala/org/agilewiki/util/lite/seq/LiteNavigableMapSeq.scala
@@ -39,14 +39,12 @@ class LiteNavigableMapSeq[T, V](reactor: LiteReactor, navigableMap: NavigableMap
     c
   }
 
-  addRequestHandler{
-    case req: SeqFirstReq => _first(req)(back)
-    case req: SeqCurrentReq[T] => _current(req)(back)
-    case req: SeqNextReq[T] => _next(req)(back)
-  }
+  bind(classOf[SeqFirstReq], _first)
+  bind(classOf[SeqCurrentReq[T]], _current)
+  bind(classOf[SeqNextReq[T]], _next)
 
-  protected def _first(req: SeqFirstReq)
-                      (responseProcess: PartialFunction[Any, Unit]) {
+  private def _first(msg: AnyRef, responseProcess: PartialFunction[Any, Unit]) {
+    val req = msg.asInstanceOf[SeqFirstReq]
     if (navigableMap.isEmpty) responseProcess(SeqEndRsp())
     else {
       val key = navigableMap.firstKey
@@ -54,8 +52,8 @@ class LiteNavigableMapSeq[T, V](reactor: LiteReactor, navigableMap: NavigableMap
     }
   }
 
-  protected def _current(req: SeqCurrentReq[T])
-                        (responseProcess: PartialFunction[Any, Unit]) {
+  private def _current(msg: AnyRef, responseProcess: PartialFunction[Any, Unit]) {
+    val req = msg.asInstanceOf[SeqCurrentReq[T]]
     if (navigableMap.isEmpty) responseProcess(SeqEndRsp())
     else {
       val key = navigableMap.ceilingKey(req.key)
@@ -66,8 +64,8 @@ class LiteNavigableMapSeq[T, V](reactor: LiteReactor, navigableMap: NavigableMap
     }
   }
 
-  protected def _next(req: SeqNextReq[T])
-                     (responseProcess: PartialFunction[Any, Unit]) {
+  private def _next(msg: AnyRef, responseProcess: PartialFunction[Any, Unit]) {
+    val req = msg.asInstanceOf[SeqNextReq[T]]
     if (navigableMap.isEmpty) responseProcess(SeqEndRsp())
     else {
       val key = navigableMap.higherKey(req.key)
