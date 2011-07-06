a55c18f1ef0d33969d4075229e86e722f3d275b6
Bill LaForge <laforge49@gmail.com>
2011-07-06 16:35:14 +0530

    GettingStarted code

---
+++ Blip/src/test/scala/org/agilewiki/blip/gettingStarted/SimpleActor.scala
@@ -0,0 +1,27 @@
+package org.agilewiki.blip
+package gettingStarted
+
+import org.specs.SpecificationWithJUnit
+
+case class Times2(value: Int)
+case class Add(value1: Int, value2: Int)
+
+class SimpleActor(mailbox: Mailbox) extends Actor(mailbox, null) {
+  bind(classOf[Times2], times2)
+  bind(classOf[Add], add)
+  private def times2(msg: AnyRef, rf: Any => Unit) { rf(msg.asInstanceOf[Times2].value * 2) }
+  private def add(msg: AnyRef, rf: Any => Unit) {
+    val req = msg.asInstanceOf[Add]
+    rf(req.value1 + req.value2)
+  }
+}
+
+class Test extends SpecificationWithJUnit {
+  "SimpleActor" should {
+    "print" in {
+      val simpleActor = new SimpleActor(null)
+      println(Future(simpleActor, Times2(21)))
+      println(Future(simpleActor, Add(1, 2)))
+    }
+  }
+}
