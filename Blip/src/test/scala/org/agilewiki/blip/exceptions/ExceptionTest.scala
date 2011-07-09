7e1110ea93d320671cac07ae535a0efae72ad467
Bill LaForge <laforge49@gmail.com>
2011-07-09 11:47:25 +0530

    more exception handler tests

--- Blip/src/test/scala/org/agilewiki/blip/exceptions/ExceptionTest.scala
+++ Blip/src/test/scala/org/agilewiki/blip/exceptions/ExceptionTest.scala
@@ -16,6 +16,9 @@ class S(mailbox: Mailbox) extends Actor(mailbox, null) {
 }
 
 case class AsyncServerEx()
+case class SyncServerEx()
+case class AsyncRspEx()
+case class SyncRspEx()
 
 class D extends Actor(new Mailbox, null) {
   bind(classOf[AsyncServerEx],{(msg, rf) =>
@@ -28,13 +31,52 @@ class D extends Actor(new Mailbox, null) {
     val s = new S(new Mailbox)
     s(SE())(rf)
   }
+
+  bind(classOf[SyncServerEx],{(msg, rf) =>
+    exceptionHandler(msg, rf, syncServerEx){exceptionHandler =>
+      println("got exception")
+      rf(null)
+    }
+  })
+  def syncServerEx(msg: AnyRef, rf: Any => Unit) {
+    val s = new S(mailbox)
+    s(SE())(rf)
+  }
+
+  bind(classOf[AsyncRspEx],{(msg, rf) =>
+    exceptionHandler(msg, rf, asyncRspEx){exceptionHandler =>
+      println("got exception")
+      rf(null)
+    }
+  })
+  def asyncRspEx(msg: AnyRef, rf: Any => Unit) {
+    val s = new S(new Mailbox)
+    s(SNE()){throw new IllegalAccessException}
+  }
+
+  bind(classOf[SyncRspEx],{(msg, rf) =>
+    exceptionHandler(msg, rf, syncRspEx){exceptionHandler =>
+      println("got exception")
+      rf(null)
+    }
+  })
+  def syncRspEx(msg: AnyRef, rf: Any => Unit) {
+    val s = new S(mailbox)
+    s(SNE()){throw new IllegalAccessException}
+  }
 }
 
 class ExceptionTest extends SpecificationWithJUnit {
   "ExceptionTest" should {
     "exercise exception handlers" in {
-      println("--no handler async test")
+      println("--server exception async test")
       Future(new D, AsyncServerEx())
+      println("--server exception sync test")
+      Future(new D, SyncServerEx())
+      println("--response exception async test")
+      Future(new D, AsyncRspEx())
+      println("--response exception sync test")
+      Future(new D, SyncRspEx())
     }
   }
 }
