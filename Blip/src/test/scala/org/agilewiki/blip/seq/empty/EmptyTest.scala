63dfb690f12df259aea40a0d462727f32e293b9e
Bill LaForge <laforge49@gmail.com>
2011-07-14 11:01:24 +0530

    EmptyTest

---
+++ Blip/src/test/scala/org/agilewiki/blip/seq/empty/EmptyTest.scala
@@ -0,0 +1,16 @@
+package org.agilewiki.blip
+package seq
+package empty
+
+import org.specs.SpecificationWithJUnit
+
+class EmptyTest extends SpecificationWithJUnit {
+  "EmptySeq" should {
+    "support first, current and next" in {
+      val emptySeq = new EmptySeq
+      println(Future(emptySeq, First()))
+      println(Future(emptySeq, Current(3)))
+      println(Future(emptySeq, Next("abc")))
+    }
+  }
+}
