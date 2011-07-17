4181c3686ad9adcdd97d864f95ea079ad99a62e8
Bill LaForge <laforge49@gmail.com>
2011-07-17 15:50:58 +0530

    MapSafeSeq with test

---
+++ Blip/src/test/scala/org/agilewiki/blip/seq/mapSafeSeq/MapSafeSeqTest.scala
@@ -0,0 +1,21 @@
+package org.agilewiki.blip
+package seq
+package mapSafeSeq
+
+import org.specs.SpecificationWithJUnit
+
+class MapSafe extends Safe {
+  override def func(msg: AnyRef, rf: Any => Unit)(implicit sender: ActiveActor) {
+    val kvPair = msg.asInstanceOf[KVPair[Int, Int]]
+    rf(kvPair.value * 2)
+  }
+}
+
+class MapSafeSeqTest extends SpecificationWithJUnit {
+  "MapSafeSeqTest" should {
+    "map" in {
+      val transform = new MapSafeSeq[Int, Int, Int](Range(0, 3), new MapSafe)
+      Future(transform, Loop((key: Int, value: Int) => println(key + " " + value)))
+    }
+  }
+}
