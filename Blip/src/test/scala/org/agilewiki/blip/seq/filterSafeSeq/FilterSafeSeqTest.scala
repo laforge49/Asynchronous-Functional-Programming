42ad1e2f5927bf0c69a93d528fe36d1fd4b79665
Bill LaForge <laforge49@gmail.com>
2011-07-27 09:07:28 +0530

    FilterSafeSeq and test

---
+++ Blip/src/test/scala/org/agilewiki/blip/seq/filterSafeSeq/FilterSafeSeqTest.scala
@@ -0,0 +1,22 @@
+package org.agilewiki.blip
+package seq
+package filterSafeSeq
+
+import org.specs.SpecificationWithJUnit
+
+class FilterSafe extends Safe {
+  override def func(msg: AnyRef, rf: Any => Unit)(implicit sender: ActiveActor) {
+    val kvPair = msg.asInstanceOf[KVPair[Int, Int]]
+    rf(kvPair.value % 2 == 0)
+  }
+}
+
+class FilterSafeSeqTest extends SpecificationWithJUnit {
+  "FilterSeqTest" should {
+    "filter" in {
+      val range = Range(0, 4)
+      val filter = new FilterSafeSeq(range, new FilterSafe)
+      Future(filter, Loop((key: Int, value: Int) => println(key+" "+value)))
+    }
+  }
+}
