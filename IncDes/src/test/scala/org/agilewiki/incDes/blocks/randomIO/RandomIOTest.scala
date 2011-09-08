9504b8e1e940c980e30861b264474e6a9f43dfe0
Bill LaForge <laforge49@gmail.com>
2011-09-08 10:11:18 +0530

    finished RandomIOTest

--- IncDes/src/test/scala/org/agilewiki/incDes/blocks/randomIO/RandomIOTest.scala
+++ IncDes/src/test/scala/org/agilewiki/incDes/blocks/randomIO/RandomIOTest.scala
@@ -1,10 +1,28 @@
 package org.agilewiki
 package incDes
 package blocks
-package blockCache
+package randomIO
 
 import blip._
 import org.specs.SpecificationWithJUnit
+
+case class DoIt()
+
+class Driver extends Actor {
+  bind(classOf[DoIt], doIt)
+
+  def doIt(msg: AnyRef, rf: Any => Unit) {
+    systemServices(WriteBytes(0, new Array[Byte](5000))) {
+      rsp1 => {
+        systemServices(ReadBytes(0, 5000)) {
+          rsp2 => {
+            rf(rsp2)
+          }
+        }
+      }
+    }
+  }
+}
 
 class RandomIOTest extends SpecificationWithJUnit {
   "RandomIOTest" should {
@@ -12,6 +30,11 @@ class RandomIOTest extends SpecificationWithJUnit {
       val properties = new java.util.TreeMap[String, String]
       properties.put("dbPathname", "RandomIOTest.db")
       val systemServices = SystemServices(new RandomIOComponentFactory, properties = properties)
+      val driver = new Driver
+      driver.setMailbox(new Mailbox)
+      driver.setSystemServices(systemServices)
+      val bytes = Future(driver, DoIt()).asInstanceOf[Array[Byte]]
+      println(bytes.length)
     }
   }
 }
