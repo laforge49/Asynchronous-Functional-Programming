b2c93cb2faebac65145fac1489976be6e5d41ec8
Bill LaForge <laforge49@gmail.com>
2011-09-23 17:09:22 +0530

    new RootSeq

--- Db/src/main/scala/org/agilewiki/db/transactions/rootStrings/GetStringRequest.scala
+++ Db/src/main/scala/org/agilewiki/db/transactions/rootStrings/GetStringRequest.scala
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2011 Bill La Forge
+ *
+ * This file is part of AgileWiki and is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License (LGPL) as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or navigate to the following url http://www.gnu.org/licenses/lgpl-2.1.txt
+ *
+ * Note however that only Scala, Java and JavaScript files are being covered by LGPL.
+ * All other files are covered by the Common Public License (CPL).
+ * A copy of this license is also included and can be
+ * found as well at http://www.opensource.org/licenses/cpl1.0.txt
+ */
 package org.agilewiki
 package db
 package transactions
@@ -8,11 +31,9 @@ import incDes._
 import blocks._
 
 object GetStringRequest {
-  def apply() = (new GetStringRequestFactory).newActor(null).
-    asInstanceOf[IncDesString]
-
   def process(db: Actor, key: String) = {
-    val je = apply()
+    val je = (new GetStringRequestFactory).newActor(null).
+    asInstanceOf[IncDes]
     val chain = new Chain
     chain.op(je, Set(null, key))
     chain.op(db, TransactionRequest(je))
@@ -37,6 +58,7 @@ class GetStringRequestComponent(actor: Actor)
   private def process(msg: AnyRef, chain: Chain) {
     chain.op(actor, Value(), "key")
     chain.op(systemServices, DbRoot(), "dbRoot")
-    chain.op(Unit => chain("dbRoot"), Unit => GetValue(chain("key")), "value")
+    chain.op(Unit => chain("dbRoot"), Value(), "strings")
+    chain.op(Unit => chain("strings"), Unit => GetValue(chain("key")))
  }
 }
