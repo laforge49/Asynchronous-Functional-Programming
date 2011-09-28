151c929f4b0f7c54e3122aafca8fd6f323528c00
Bill LaForge <laforge49@gmail.com>
2011-09-28 08:20:34 +0530

    SizeTest woiks

---
+++ Db/src/test/scala/org/agilewiki/db/transactions/size/SizeTest.scala
@@ -0,0 +1,44 @@
+package org.agilewiki
+package db
+package transactions
+package size
+
+import blip._
+import services._
+import incDes._
+import org.specs.SpecificationWithJUnit
+
+class SizeTest extends SpecificationWithJUnit {
+  "SizeTest" should {
+    "get size" in {
+      val systemServices = SystemServices(new ServicesRootComponentFactory)
+      val dbName = "SizeNoLog.db"
+      val file = new java.io.File(dbName)
+      file.delete
+      val properties = new Properties
+      properties.put("dbPathname", dbName)
+      val db = Subsystem(
+        systemServices,
+        new SmallNoLogComponentFactory,
+        properties = properties,
+        actorId = ActorId("db"))
+      val chain = new Chain
+      chain.op(systemServices, Register(db))
+      chain.op(db, SetRequest.process(db, "/$", IncDesLongIncDesMap(null, db)))
+      chain.op(db, SetRequest.process(db, "/$/-111111111111111111",
+        IncDesIncDes(null)))
+      chain.op(db, SetRequest.process(db, "/$/-111111111111111111/$",
+        IncDesStringSet(null, db)))
+      chain.op(db, SetRequest.process(db, "/$/0",
+        IncDesIncDes(null)))
+      chain.op(db, SetRequest.process(db, "/$/0/$",
+        IncDesBytesList(null, db)))
+      chain.op(db, SizeRequest.process(db, "/$"), "mapSize")
+      chain.op(db, SizeRequest.process(db, "/$/-111111111111111111/$"), "setSize")
+      chain.op(db, SizeRequest.process(db, "/$/0/$"), "listSize")
+      Future(systemServices, chain)
+      println(chain.results)
+      systemServices.close
+    }
+  }
+}
