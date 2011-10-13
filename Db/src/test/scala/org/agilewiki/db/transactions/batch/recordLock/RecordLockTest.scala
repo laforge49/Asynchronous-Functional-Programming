98858fc16e75ddf2f97608551f867f6da2081909
Bill LaForge <laforge49@gmail.com>
2011-10-13 18:53:44 +0530

    RecordLockTest

---
+++ Db/src/test/scala/org/agilewiki/db/transactions/batch/recordLock/RecordLockTest.scala
@@ -0,0 +1,122 @@
+package org.agilewiki
+package db
+package transactions
+package batch
+package recordLock
+
+import blip._
+import services._
+import log._
+import incDes._
+import org.specs.SpecificationWithJUnit
+
+case class Counter(db: Actor) {
+  def init = {
+    val counter = IncDesInt(null)
+    val batch = Batch(db)
+    val chain = new Chain
+    chain.op(db, NewRecord(batch, "r1"))
+    chain.op(db, RecordUpdate(batch, "r1", "$", counter))
+    chain.op(db, TransactionRequest(batch))
+    chain
+  }
+
+  def buildAdd(n: Int) = {
+    var counter: IncDesInt = null
+    val batch = Batch(db)
+    val chain = new Chain
+    chain.op(db, RecordGet(db, batch, "r1", "$"), "counter")
+    chain.op(Unit => {
+      counter = chain("counter").asInstanceOf[IncDesInt]
+      counter
+    }, Value(), "value")
+    chain.op(Unit => counter, Unit => {
+      val value = chain("value").asInstanceOf[Int]
+      Set(null, value + n)
+    })
+    chain.op(db, Unit => RecordUpdate(batch, "r1", "$", counter))
+    (chain, TransactionRequest(batch))
+  }
+
+  def get = {
+    val chain = new Chain
+    chain.op(db, RecordGet(db, null, "r1", "$"), "counter")
+    chain.op(Unit => chain("counter").asInstanceOf[IncDesInt], Value())
+    chain
+  }
+}
+
+class RecordLockTest extends SpecificationWithJUnit {
+  "RecordLockTest" should {
+    "add 1 to counter" in {
+      val systemServices = SystemServices(new ServicesRootComponentFactory)
+      val dbName = "recordLock.db"
+      val logDirPathname = "recordLock"
+      val file = new java.io.File(dbName)
+      file.delete
+      EmptyLogDirectory(logDirPathname)
+      val properties = new Properties
+      properties.put("dbPathname", dbName)
+      properties.put("logDirPathname", logDirPathname)
+      properties.put("flushLog", "true")
+      val db = Subsystem(
+        systemServices,
+        new SmallRecordsComponentFactory,
+        properties = properties,
+        actorId = ActorId("db"))
+      val chain = new Chain
+      chain.op(systemServices, Register(db))
+      chain.op(db, Counter(db).init)
+      chain.op(db, Counter(db).get, "oldCounterValue")
+      val (b1, t1) = Counter(db).buildAdd(1)
+      chain.op(db, b1)
+      chain.op(db, t1)
+      chain.op(db, Counter(db).get, "newCounterValue")
+      Future(systemServices, chain)
+      println(chain.results)
+      systemServices.close
+    }
+    "force a conflict" in {
+      val systemServices = SystemServices(new ServicesRootComponentFactory)
+      val dbName = "recordLock.db"
+      val logDirPathname = "recordLock"
+      val properties = new Properties
+      properties.put("dbPathname", dbName)
+      properties.put("logDirPathname", logDirPathname)
+      properties.put("flushLog", "true")
+      val db = Subsystem(
+        systemServices,
+        new SmallRecordsComponentFactory,
+        properties = properties,
+        actorId = ActorId("db"))
+      val chain = new Chain
+      chain.op(systemServices, Register(db))
+      chain.op(db, Counter(db).get, "oldCounterValue")
+      val (b1, t1) = Counter(db).buildAdd(1)
+      val (b2, t2) = Counter(db).buildAdd(1)
+      chain.op(db, b1)
+      chain.op(db, b2)
+      chain.op(db, t1)
+      Future(systemServices, chain)
+      println(chain.results)
+      try {
+        println(Future(db, t2))
+        println("can't get here")
+      } catch {
+        case ex: TransactionConflictException => {
+          ex.printStackTrace
+          println("retrying")
+          val retry = new Chain
+          val (b3, t3) = Counter(db).buildAdd(1)
+          retry.op(db, b3)
+          retry.op(db, t3)
+          Future(db, retry)
+          println("retry was successful")
+        }
+        case ex: Exception => throw ex
+      }
+      println(Future(db, Counter(db).get))
+      systemServices.close
+    }
+  }
+}
