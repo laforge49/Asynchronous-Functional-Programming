0db619962d641faeca586b78e94df065f689b30e
Bill LaForge <laforge49@gmail.com>
2011-10-28 08:21:19 +0530

    Added some comments.

--- Blip/src/main/scala/org/agilewiki/blip/ConcurrentLinkedBlockingQueue.scala
+++ Blip/src/main/scala/org/agilewiki/blip/ConcurrentLinkedBlockingQueue.scala
@@ -3,12 +3,16 @@ package org.agilewiki.blip
 import java.util.concurrent._
 import annotation.tailrec
 
+/**
+ * A ConcurrentLinkedQueue with a take method that doesn't block when the queue isn't empty.
+ * Note that this code only supports a singe reader thread.
+ */
 class ConcurrentLinkedBlockingQueue[E]
   extends ConcurrentLinkedQueue[E] {
-  private val ab = new atomic.AtomicBoolean
-  private val s = new Semaphore(1)
+  private val ab = new atomic.AtomicBoolean //when true, take is requesting a permit
+  private val s = new Semaphore(1) //to wake up a pending take
 
-  s.drainPermits
+  s.drainPermits //start with no permits
 
   def put(e: E) {
     offer(e)
@@ -16,20 +20,21 @@ class ConcurrentLinkedBlockingQueue[E]
 
   override def offer(e: E) = {
     super.offer(e)
-    if (ab.compareAndSet(true, false)) s.release
+    if (ab.compareAndSet(true, false)) s.release //if there is a pending take, wake it up
     true
   }
 
   @tailrec final def take(): E = {
     var rv = poll
     if (rv != null) return rv
+    //the queue may now be empty, so request a permit
     ab.set(true)
     rv = poll
-    if (rv != null) {
-      if (!ab.compareAndSet(true, false)) s.drainPermits
+    if (rv != null) { //the queue was not empty
+      if (!ab.compareAndSet(true, false)) s.drainPermits //clear the permit that we didn't need
       return rv
     }
-    s.acquire
+    s.acquire //wait for a permit
     take
   }
 }
