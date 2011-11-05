69b3d4248578be80d24d95e45ecec7cb1b02c28a
Bill LaForge <laforge49@gmail.com>
2011-11-05 12:39:19 +0530

    LoopTest now uses mailboxFactory

--- Blip/src/test/scala/org/agilewiki/blip/loops/LoopTest.scala
+++ Blip/src/test/scala/org/agilewiki/blip/loops/LoopTest.scala
@@ -4,10 +4,12 @@ package loops
 import org.specs.SpecificationWithJUnit
 
 case class Loop(n: Int)
+
 case class I(i: Int)
 
 class P extends Actor {
-  bind (classOf[I], ifunc)
+  bind(classOf[I], ifunc)
+
   def ifunc(msg: AnyRef, rf: Any => Unit) {
     println(msg.asInstanceOf[I].i)
     rf(null)
@@ -16,6 +18,7 @@ class P extends Actor {
 
 class L(a: Actor) extends Actor {
   bind(classOf[Loop], lfunc)
+
   def lfunc(msg: AnyRef, rf: Any => Unit) {
     val n = msg.asInstanceOf[Loop].n
     if (n < 1) {
@@ -26,9 +29,10 @@ class L(a: Actor) extends Actor {
     var r = 0
     while (i < n) {
       i += 1
-      a(I(i)) { rsp =>
-        r += 1
-        if (r == n) rf(null)
+      a(I(i)) {
+        rsp =>
+          r += 1
+          if (r == n) rf(null)
       }
     }
   }
@@ -37,17 +41,22 @@ class L(a: Actor) extends Actor {
 class LoopTest extends SpecificationWithJUnit {
   "LoopTest" should {
     "print 1, 2, 3 twice" in {
-      val mb = new ReactorMailbox
-      val p = new P
-      p.setMailbox(mb)
-      println("synchronous test")
-      val sl = new L(p)
-      sl.setMailbox(mb)
-      Future(sl, Loop(3))
-      println("asynchronous test")
-      val al = new L(p)
-      al.setMailbox(new ReactorMailbox)
-      Future(al, Loop(3))
+      val mailboxFactory = new MailboxFactory
+      try {
+        val mb = mailboxFactory.asyncMailbox
+        val p = new P
+        p.setMailbox(mb)
+        println("synchronous test")
+        val sl = new L(p)
+        sl.setMailbox(mb)
+        Future(sl, Loop(3))
+        println("asynchronous test")
+        val al = new L(p)
+        al.setMailbox(mailboxFactory.asyncMailbox)
+        Future(al, Loop(3))
+      } finally {
+        mailboxFactory.close
+      }
     }
   }
 }
