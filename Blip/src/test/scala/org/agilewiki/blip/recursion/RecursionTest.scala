e394c377be063c1d8716b28a067f22e04e5f69d5
Bill LaForge <laforge49@gmail.com>
2011-11-05 12:43:06 +0530

    RecursionTest now uses MailboxFactory

--- Blip/src/test/scala/org/agilewiki/blip/recursion/RecursionTest.scala
+++ Blip/src/test/scala/org/agilewiki/blip/recursion/RecursionTest.scala
@@ -5,10 +5,12 @@ import org.specs.SpecificationWithJUnit
 import annotation.tailrec
 
 case class Loop(n: Int)
+
 case class I(i: Int)
 
 class P extends Actor {
-  bind (classOf[I], ifunc)
+  bind(classOf[I], ifunc)
+
   def ifunc(msg: AnyRef, rf: Any => Unit) {
     println(msg.asInstanceOf[I].i)
     rf(null)
@@ -17,6 +19,7 @@ class P extends Actor {
 
 class L(a: Actor) extends Actor {
   bind(classOf[Loop], lfunc)
+
   def lfunc(msg: AnyRef, rf: Any => Unit) {
     val n = msg.asInstanceOf[Loop].n
     if (n < 1) {
@@ -30,10 +33,11 @@ class L(a: Actor) extends Actor {
     var async = false
     var sync = false
     val i = _i + 1
-    a(I(i)) {rsp =>
-      if (i == n) rf(null)
-      else if (async) _lrec(n, i, rf)
-      else sync = true
+    a(I(i)) {
+      rsp =>
+        if (i == n) rf(null)
+        else if (async) _lrec(n, i, rf)
+        else sync = true
     }
     if (!sync) {
       async = true
@@ -42,22 +46,29 @@ class L(a: Actor) extends Actor {
     lrec(n, i, rf)
   }
 
-  def _lrec(n: Int, i: Int, rf: Any => Unit) {lrec(n, i, rf)}
+  def _lrec(n: Int, i: Int, rf: Any => Unit) {
+    lrec(n, i, rf)
+  }
 }
 
 class RecursionTest extends SpecificationWithJUnit {
   "RecursionTest" should {
     "print 1, 2, 3 twice" in {
-      val mb = new ReactorMailbox
-      val p = new P
-      println("synchronous test")
-      val sl = new L(p)
-      sl.setMailbox(mb)
-      Future(sl, Loop(3))
-      println("asynchronous test")
-      val al = new L(p)
-      al.setMailbox(new ReactorMailbox)
-      Future(al, Loop(3))
+      val mailboxFactory = new MailboxFactory
+      try {
+        val mb = mailboxFactory.asyncMailbox
+        val p = new P
+        println("synchronous test")
+        val sl = new L(p)
+        sl.setMailbox(mb)
+        Future(sl, Loop(3))
+        println("asynchronous test")
+        val al = new L(p)
+        al.setMailbox(mailboxFactory.asyncMailbox)
+        Future(al, Loop(3))
+      } finally {
+        mailboxFactory.close
+      }
     }
   }
 }
