de83452ad13313f805225ea407bea5ab9e7f4b34
Bill LaForge <laforge49@gmail.com>
2011-11-06 16:32:08 +0530

    Blip tests no longer use ReactorMailbox.

--- Blip/src/test/scala/org/agilewiki/blip/ioc/IOCTest.scala
+++ Blip/src/test/scala/org/agilewiki/blip/ioc/IOCTest.scala
@@ -5,9 +5,12 @@ import org.specs.SpecificationWithJUnit
 
 case class Today()
 
-class Sayings(actor: Actor) extends Component(actor){
+class Sayings(actor: Actor) extends Component(actor) {
   bind(classOf[Today], today)
-  def today(msg: AnyRef, rf: Any => Unit) {rf("Today is the first day of the rest of your life.")}
+
+  def today(msg: AnyRef, rf: Any => Unit) {
+    rf("Today is the first day of the rest of your life.")
+  }
 }
 
 class SayingsFactory extends ComponentFactory {
@@ -17,8 +20,8 @@ class SayingsFactory extends ComponentFactory {
 case class SaySomething()
 
 class SayIt extends Actor {
-  setMailbox(new ReactorMailbox)
   bind(classOf[SaySomething], saySomething)
+
   def saySomething(msg: AnyRef, rf: Any => Unit) {
     systemServices(Today())(rf)
   }
@@ -28,9 +31,14 @@ class IOCTest extends SpecificationWithJUnit {
   "SimpleActor" should {
     "print" in {
       val systemServices = SystemServices(new SayingsFactory)
-      val sayIt = new SayIt
-      sayIt.setSystemServices(systemServices)
-      println(Future(sayIt, SaySomething()))
+      try {
+        val sayIt = new SayIt
+        sayIt.setSystemServices(systemServices)
+        sayIt.setMailbox(systemServices.newSyncMailbox)
+        println(Future(sayIt, SaySomething()))
+      } finally {
+        systemServices.close
+      }
     }
   }
 }
